2a8376fc3281e4dc06f387dbb01855778102d38f
jiangda@didichuxing.com <jiangda@didichuxing.com>
2016-10-09 01:08:09 +0800

    change utf-8 encoding

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-module.exports = router;
+module.exports = function(app){
+  app.get('/', function(req, res, next) {
+    res.render('index', { title: '本域名出售，qq:780228437,email:jiangda1@foxmail.com' });
+  });
+};
